ff55cf0ee6f4c7c643b4edb62ad6202cd7803791
xawk <xawk0469@yandex.ru>
2024-08-27 17:32:19 +0300

    one move

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,17 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class DinnerConstructor {
 
+    HashMap<String, ArrayList<String>> foodList;
+    Random random = new Random();
+
+    void addNewDish(String dishType, String dishName) {
+        ArrayList<String>
+    }
+
 }
+
